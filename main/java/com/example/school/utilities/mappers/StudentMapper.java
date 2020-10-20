4f54ae5eb3fbb3d72479eaccdf69292dfc2c5d67
Nydox <cbotushanov@gmail.com>
2020-10-20 17:52:52 +0300

    Added a student mapper

---
+++ main/java/com/example/school/utilities/mappers/StudentMapper.java
@@ -0,0 +1,36 @@
+package com.example.school.utilities.mappers;
+
+import java.util.ArrayList;
+
+import com.example.school.database.entities.Student;
+import com.example.school.viewModels.StudentViewModel;
+
+public class StudentMapper {
+    public static Iterable<StudentViewModel> mapStudentsToViewModels(Iterable<Student> students) {
+        ArrayList<StudentViewModel> result = new ArrayList<>();
+
+        for (Student student : students) {
+            result.add(mapEntityTViewModel(student));
+        }
+
+        return result;
+    }
+
+    public static StudentViewModel mapEntityTViewModel(Student student) {
+        StudentViewModel result = new StudentViewModel();
+        
+        result = generateViewModel(student);
+
+        return result;
+    }
+
+    private static StudentViewModel generateViewModel(Student student) {
+        StudentViewModel result = new StudentViewModel();
+
+        result.setEmail(student.getEmail());
+        result.setId(Long.toString(student.getStudentId()));
+        result.setName(student.getName());
+        
+        return result;
+    }
+}
